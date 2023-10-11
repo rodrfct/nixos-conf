77aec94e7c6e837ef77aae81c0a7649ccf7f77ac
rodrfct <rodrigoca.fct@gmail.com>
2023-10-11 17:37:00 +0200

    Add lualine and toggleterm

--- home-manager/develop/nvim/neovim.nix
+++ home-manager/develop/nvim/neovim.nix
@@ -29,9 +29,15 @@
         config = "colorscheme nord";
       }
       {
+        plugin = lualine-nvim;
+        type = "lua";
+        config = "require('lualine').setup()";
+      }
+      nvim-web-devicons
+      {
         plugin = comment-nvim;
         type = "lua";
-        config = "require(\"Comment\").setup()";
+        config = "require('Comment').setup()";
       }
       {
         plugin = gitsigns-nvim;
@@ -50,6 +56,30 @@
         plugin = nvim-autopairs;
         type = "lua";
         config = "require('nvim-autopairs').setup()";
+      }
+      {
+        plugin = toggleterm-nvim;
+        type = "lua";
+        config = ''
+        require('toggleterm').setup{
+          start_in_insert = true,
+          hide_numbers = true,
+        }
+
+        vim.keymap.set('n', '<leader>tt', ':ToggleTerm<CR>', { buffer = bufnr })
+
+        function _G.set_terminal_keymaps()
+          local opts = {buffer = 0}
+          vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+          vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+          vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+          vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+          vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+          vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+        end
+
+        vim.cmd('autocmd! TermOpen term://*toggleterm#* lua set_terminal_keymaps()')
+        '';
       }
       
       luasnip
@@ -117,6 +147,13 @@
     vim.o.termguicolors = true
 
     vim.o.mouse = 'a'
+
+    local bufmap = function(keys, func)
+      vim.keymap.set('n', keys, func, { buffer = bufnr })
+    end
+  
+    bufmap('<leader>fn', ':tabn<CR>')
+    bufmap('<leader>fp', ':tabp<CR>')
 
     '';
   };
