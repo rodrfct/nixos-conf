3388833bf962403bde74aff83ae642856fa2062d
rodrfct <rodrigoca.fct@gmail.com>
2023-11-09 13:45:14 +0100

    Tidy files and install some fonts

--- home-manager/clase/pandoc/default.nix
+++ home-manager/clase/pandoc/default.nix
@@ -9,30 +9,16 @@
   home.packages = with pkgs; [
     python311Packages.weasyprint
     (import ./akdmik.nix {inherit pkgs inputs;})
+    # Fonts
+    prociono # Roman replacement
+    fanwood # The one I personally like the most
+    orbitron # I finally found the libre Zekton clone
   ];
 
-  home.file."css" = {
+  home.file."pandoc" = {
     recursive = true;
-    source = "${inputs.pandoc-setup}/css";
-    target = ".local/share/pandoc/css";
-  };
-  
-  home.file."js" = {
-    recursive = true;
-    source = "${inputs.pandoc-setup}/js";
-    target = ".local/share/pandoc/js";
-  };
-
-  home.file."templates" = {
-    recursive = true;
-    source = "${inputs.pandoc-setup}/templates";
-    target = ".local/share/pandoc/templates";
-  };
-
-  home.file."defaults" = {
-    recursive = true;
-    source = "${inputs.pandoc-setup}/defaults";
-    target = ".local/share/pandoc/defaults";
+    source = "${inputs.pandoc-setup}";
+    target = ".local/share/pandoc";
   };
 
 }
