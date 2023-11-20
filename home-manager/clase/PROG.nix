4423f060b5d331c2ae69a81a1b33aa2633675e66
rodrfct <rodrigoca.fct@gmail.com>
2023-11-20 17:30:08 +0100

    Add Java LSP

--- home-manager/clase/PROG.nix
+++ home-manager/clase/PROG.nix
@@ -7,5 +7,30 @@
 
   home.packages = [
     pkgs.netbeans
+    pkgs.gradle
   ];
+
+  programs.neovim = {
+    extraPackages = with pkgs; [
+      java-language-server
+    ];
+
+    plugins = with pkgs.vimPlugins; [
+      {
+        plugin = nvim-lspconfig;
+        type = "lua";
+        config = ''
+        require('lspconfig').java_language_server.setup {
+          on_attach = on_attach,
+          capabilities = capabilities,
+          root_dir = function()
+            return vim.loop.cwd()
+          end,
+          
+          cmd = {"java-language-server"},
+        }
+        '';
+      }
+    ];
+  };
 }
