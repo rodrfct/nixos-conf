916a45ec2c68366222cd287787a6f0387bc1c211
rodrfct <rodrigoca.fct@gmail.com>
2023-11-24 17:18:27 +0100

    Fix LSP integration

--- home-manager/clase/PROG/PROG.nix
+++ home-manager/clase/PROG/PROG.nix
@@ -22,6 +22,34 @@
         plugin = nvim-lspconfig;
         type = "lua";
         config = ''
+        local on_attach = function(_, bufnr)
+
+          local bufmap = function(keys, func)
+            vim.keymap.set('n', keys, func, { buffer = bufnr })
+          end
+
+          bufmap('<leader>r', vim.lsp.buf.rename)
+          bufmap('<leader>a', vim.lsp.buf.code_action)
+
+          bufmap('gd', vim.lsp.buf.definition)
+          bufmap('gD', vim.lsp.buf.declaration)
+          bufmap('gI', vim.lsp.buf.implementation)
+          bufmap('<leader>D', vim.lsp.buf.type_definition)
+
+          bufmap('gr', require('telescope.builtin').lsp_references)
+          bufmap('<leader>s', require('telescope.builtin').lsp_document_symbols)
+          bufmap('<leader>S', require('telescope.builtin').lsp_dynamic_workspace_symbols)
+
+          bufmap('K', vim.lsp.buf.hover)
+
+          vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+            vim.lsp.buf.format()
+          end, {})
+        end
+
+        local capabilities = vim.lsp.protocol.make_client_capabilities()
+        capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+
         -- Java LS
         require('lspconfig').java_language_server.setup {
           on_attach = on_attach,
@@ -34,7 +62,10 @@
         }
 
         -- Kotlin LS
-        require'lspconfig'.kotlin_language_server.setup{}
+        require'lspconfig'.kotlin_language_server.setup {
+          on_attach = on_attach,
+          capabilities = capabilities,
+        }
         '';
       }
       nvim-treesitter-parsers.java
