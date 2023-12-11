1f3d1b731bf50a3b34309d41b450a277189fb4ab
rodrfct <rodrigoca.fct@gmail.com>
2023-12-11 12:26:25 +0100

    Remove some comments

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -10,21 +10,6 @@
   home.username = "rodrigo";
   home.homeDirectory = "/home/rodrigo";
 
-  # link the configuration file in current directory to the specified location in home directory
-  # home.file.".config/i3/wallpaper.jpg".source = ./wallpaper.jpg;
-
-  # link all files in `./scripts` to `~/.config/i3/scripts`
-  # home.file.".config/i3/scripts" = {
-  #   source = ./scripts;
-  #   recursive = true;   # link recursively
-  #   executable = true;  # make all files executable
-  # };
-
-  # encode the file content in nix configuration file directly
-  # home.file.".xxx".text = ''
-  #     xxx
-  # '';
-
   # Packages that should be installed to the user profile.
   home.packages = with pkgs; [
     btop
