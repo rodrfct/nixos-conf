360f857cccf658f10e087f2aa53e34d52d96f876
rodrfct <rodrigoca.fct@gmail.com>
2025-03-16 19:39:37 +0100

    Remove warnings

--- modules/home-manager/develop/vscode.nix
+++ modules/home-manager/develop/vscode.nix
@@ -9,57 +9,60 @@
   programs.vscode = {
   	enable = true;
   	package = pkgs.vscodium;
+    
+    profiles.default = {
 
-  	userSettings = {
-  	  "editor.fontFamily" = "'Fira Code', 'monospace', monospace";
-  	  "editor.fontLigatures" = true;
-  	  "window.menuBarVisibility" = "toggle";
-  	  "workbench.colorTheme" = "Vitesse Dark";
-  	  "workbench.iconTheme" = "material-icon-theme";
-  	  "workbench.startupEditor" = "none";
-  	  # Extensions
-  	  "markdown-preview-enhanced.previewTheme" = "github-dark.css";
+      userSettings = {
+        "editor.fontFamily" = "'Fira Code', 'monospace', monospace";
+        "editor.fontLigatures" = true;
+        "window.menuBarVisibility" = "toggle";
+        "workbench.colorTheme" = "Vitesse Dark";
+        "workbench.iconTheme" = "material-icon-theme";
+        "workbench.startupEditor" = "none";
+        # Extensions
+        "markdown-preview-enhanced.previewTheme" = "github-dark.css";
 
-      # Nix LSP
-      "nix.enableLanguageServer" = true;
-      #"nix.serverPath" = "rnix-lsp";
-      "nix.serverPath" = "nil";
-      #"nix.serverPath": "nixd"
-  	};
+        # Nix LSP
+        "nix.enableLanguageServer" = true;
+        #"nix.serverPath" = "rnix-lsp";
+        "nix.serverPath" = "nil";
+        #"nix.serverPath": "nixd"
+      };
 
-    extensions = with inputs.nix-vscode-extensions.extensions.x86_64-linux; [
-      # Language support
-      open-vsx.jnoortheen.nix-ide
-      open-vsx.vue.volar
-      # Utilities
-      open-vsx.shd101wyy.markdown-preview-enhanced
-      open-vsx.sndst00m.vscode-native-svg-preview
-      open-vsx.yandeu.five-server
-      # Color themes
-      open-vsx.antfu.theme-vitesse
-      open-vsx.teabyii.ayu
-      # Icon themes
-      open-vsx.pkief.material-icon-theme
-    ];
+      extensions = with inputs.nix-vscode-extensions.extensions.x86_64-linux; [
+        # Language support
+        open-vsx.jnoortheen.nix-ide
+        open-vsx.vue.volar
+        # Utilities
+        open-vsx.shd101wyy.markdown-preview-enhanced
+        open-vsx.sndst00m.vscode-native-svg-preview
+        open-vsx.yandeu.five-server
+        # Color themes
+        open-vsx.antfu.theme-vitesse
+        open-vsx.teabyii.ayu
+        # Icon themes
+        open-vsx.pkief.material-icon-theme
+      ];
 
-    languageSnippets = {
-      vue = {
-        "Script setup with Typescript" = {
-          "prefix" = "script:ts";
-          "body" = [
-            "<script setup lang=\"ts\">"
-            "$0"
-            "</script>"
-          ];
-        };
+      languageSnippets = {
+        vue = {
+          "Script setup with Typescript" = {
+            "prefix" = "script:ts";
+            "body" = [
+              "<script setup lang=\"ts\">"
+              "$0"
+              "</script>"
+            ];
+          };
 
-        "Template" = {
-          "prefix" = "template";
-          "body" = [
-            "<template>"
-            "\t$0"
-            "</template>"
-          ];
+          "Template" = {
+            "prefix" = "template";
+            "body" = [
+              "<template>"
+              "\t$0"
+              "</template>"
+            ];
+          };
         };
       };
 
--- modules/nixos/configuration.nix
+++ modules/nixos/configuration.nix
@@ -82,7 +82,7 @@
   console.keyMap = "es";
 
   # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
+  services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
