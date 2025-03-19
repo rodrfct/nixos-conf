76a8f5b00de2e9c2778b213b665f677c1ce7c3d0
rodrfct <rodrigoca.fct@gmail.com>
2025-03-19 21:03:28 +0100

    Rice a bit

--- modules/nixos/stylix.nix
+++ modules/nixos/stylix.nix
@@ -1,13 +1,26 @@
-{ pkgs, ...}:
+{ pkgs, config, ...}:
 
 {
   stylix = {
     enable = true;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+    # base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/nord.yaml";
 
     polarity = "dark";
 
+    opacity = {
+      applications = 1.0;
+      terminal = 0.85;
+      desktop = config.stylix.opacity.terminal;
+      popups = config.stylix.opacity.terminal;
+    };
+
     fonts = {
+      serif = config.stylix.fonts.sansSerif;
+      sansSerif = {
+        package = pkgs.noto-fonts;
+        name = "Noto";
+      };
       monospace = {
         package = pkgs.nerd-fonts.fira-code;
         name = "Fira Code Nerdfont";
