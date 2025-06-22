1efb04cc3a33c082f7ddfec2e57af91b5c801465
rodrfct <rodrigoca.fct@gmail.com>
2025-06-22 15:21:14 +0200

    Enable bluetooth

--- hosts/taillow/hardware-configuration.nix
+++ hosts/taillow/hardware-configuration.nix
@@ -31,6 +31,8 @@
 
   swapDevices = [ ];
 
+  hardware.bluetooth.enable = true;
+
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
