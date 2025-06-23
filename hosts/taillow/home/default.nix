417033132743981c365b797230a11f7263c9ffb1
rodrfct <rodrigoca.fct@gmail.com>
2025-06-23 20:55:11 +0200

    Disable hyprland

--- hosts/taillow/home/default.nix
+++ hosts/taillow/home/default.nix
@@ -3,7 +3,7 @@
 {
   imports = [
     ../../../modules/home-manager/home.nix
-    ../../../modules/home-manager/wm/hyprland/hyprland.nix
+    # ../../../modules/home-manager/wm/hyprland/hyprland.nix
     ../../../modules/home-manager/wm/waybar/layouts/default.nix
     ../../../modules/home-manager/develop/develop.nix
   ];
