7301fb8dc703384cbecae952e2f4182c1462dd2f
rodrfct <rodrigoca.fct@gmail.com>
2026-03-04 11:13:23 +0100

    Configure VSCode for work
    Sadly :(

--- modules/home-manager/develop/vscode.nix
+++ modules/home-manager/develop/vscode.nix
@@ -1,5 +1,14 @@
 { pkgs, inputs, ... }:
 
+let
+  resetLicense =
+    drv:
+    drv.overrideAttrs (prev: {
+      meta = prev.meta // {
+        license = [ ];
+      };
+    });
+in
 {
   home.packages = with pkgs; [
     nil # Nix LSP
@@ -10,7 +19,7 @@
 
   programs.vscode = {
   	enable = true;
-  	package = pkgs.vscodium;
+  	# package = pkgs.vscodium;
     
     profiles.default = {
 
@@ -29,12 +38,17 @@
         #"nix.serverPath" = "rnix-lsp";
         "nix.serverPath" = "nil";
         #"nix.serverPath": "nixd"
+        # Copilot
+        # "github.copilot.nextEditSuggestions.enabled" = true;
+        "remote.SSH.localServerDownload" = "off";
+        "remote.SSH.useLocalServer" = false;
       };
 
       extensions = with inputs.nix-vscode-extensions.extensions.x86_64-linux; [
         # Language support
         open-vsx.jnoortheen.nix-ide
         open-vsx.vue.volar
+        vscode-marketplace-release.tailwindcssintellisenseplus.vscode-tailwindcss-plus
         # Utilities
         open-vsx.shd101wyy.markdown-preview-enhanced
         open-vsx.sndst00m.vscode-native-svg-preview
@@ -44,6 +58,9 @@
         open-vsx.teabyii.ayu
         # Icon themes
         open-vsx.pkief.material-icon-theme
+        # Enterprise stuff
+        (resetLicense vscode-marketplace-release.github.copilot)
+        (resetLicense vscode-marketplace-release.github.copilot-chat)
       ];
 
       languageSnippets = {
